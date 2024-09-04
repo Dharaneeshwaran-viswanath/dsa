4ad5132f95c0ba0ee702943c420b30a85265332a
Dharaneeshwaran-viswanath <717822s112@kce.ac.in>
2024-09-03 23:42:44 -0400

    Create onetohundred.java

---
+++ onetohundred.java
@@ -0,0 +1,14 @@
+package placement3;
+
+public class onetohundred { 
+	public static void main(String[] args)
+	{
+		
+	for(int i=1;i<=100;i++)
+	{
+		if(i%2==0) {
+			system.out.println(i);
+		}
+	}
+	}
+	}
